293234f817ace90a2fe9c7cbfdcb27f42ed6ee7a
svrahul <rahul.satuluri@gmail.com>
2020-05-19 16:09:39 +0530

    Adding the core state control code
    Adding the core state control code

---
+++ state_control/StateControl.cpp
@@ -0,0 +1,197 @@
+#include "StateControl.h"
+#include "../sensors/sensors.h"
+
+bool bSendInitCommand = false;
+bool minPressureForMaskOn = false;
+bool bSendPeakHighDetected = false;
+bool bSendPeepLowDetected = false;
+bool bBreathDetectedFlag = false;
+
+// Control statemachine gloabl variable
+ControlStatesDef_T geCtrlState = CTRL_INIT;
+ControlStatesDef_T geCtrlPrevState  = CTRL_INIT;
+
+/*!< Commands array contains static command values to be sent to ventilator master */
+
+static const String commands[] =
+{ "$VMST0000&",        /**< stop stepper motor*/
+  "$VMST0001&",        /**< start stepper motor*/
+  "$VMO20100&",        /**< oxygen solenoid cylinder line off*/
+  "$VMO20101&",       /**< oxygen solenoid cylinder line on*/
+  "$VMO20200&",       /**< oxygen solenoid hospital line off*/
+  "$VMO20201&",       /**< oxygen solenoid hospital line on*/
+  "$VMSV0100&",       /**< inhalation  solenoid off*/
+  "$VMSV0101&",       /**< inhalation  solenoid on*/
+  "$VMSV0200&",       /**< exhalation  solenoid off*/
+  "$VMSV0201&",       /**< exhalation  solenoid on*/
+  "$VMSV0300&",       /**< peak pressure relief off*/
+  "$VMSV0301&",       /**< peak pressure relief on*/
+  "$VMP1xxxx&",       /**< set tidal volume */
+  "$VMP2xxxx&",       /**< set BPM */
+  "$VMP3xxxx&",       /**< set peak pressure*/
+  "$VMP4xxxx&",       /**< set Fio2*/   
+  "$VMP5xxxx&",       /**< set inhalation exhalation ratio */
+  "$VMPP0000&",       /**< slave entering parameter edit */
+  "$VMPP1111&",       /**< slave completed parameter edit */
+  "$VMIN0000&",       /**< initialize master*/
+  "$VMIN0001&",       /**< initialize stepper module*/
+  "$VMIN0002&",       /**< initialize stepper module*/
+  "$VMIN0003&"        /**< initialize breath detection*/
+};
+
+String serial2_rxdata = "";
+
+int Ctrl_send_packet(int cmdIndex) {
+	Serial2.print(commands[cmdIndex]);
+	return 0;
+}
+
+int Ctrl_send_packet(String name, int value) {
+	String param = "";
+	String command = "";
+	if (name == tidl_volu.parm_name) {
+	   param = PARAM1;
+	} else if (name == inex_rati.parm_name) {
+       param = PARAM5;
+    } else if (name == resp_rate.parm_name) {
+	   param = PARAM2;
+	} else {
+	   Serial.println("ERROR: Trying to send invalid packet");
+	   return -1;
+	}
+    command = Ctrl_CreateCommand(param, value);
+    Serial2.print(command);
+	return 0;
+}
+
+void Ctrl_store_received_packet(String data) {
+	serial2_rxdata = data;
+}
+
+void Ctrl_ProcessRxData(void) {
+  String p1;
+  String p2;
+  String payload;
+  String command;
+
+  p1 = serial2_rxdata.substring(1, 3);
+  p2 = serial2_rxdata.substring(3, 5);
+  payload = serial2_rxdata.substring(5, 9);
+  int value;
+
+  if (p1 == VENTSLAVE) {
+    if (p2 == SYNCH) {
+      geCtrlState = payload.toInt();
+    } else {
+      int index;
+      index =  payload.toInt();
+
+      if (index < MAX_CTRL_PARAMS) {
+        value = params[index].value_curr_mem;
+        command = Ctrl_CreateCommand(p2, value);
+        Serial2.print(command);
+      }
+    }
+  }
+}
+
+/*
+   Function to build the command to be sent to Ventilator Master
+*/
+String Ctrl_CreateCommand(String paramName, int value) {
+  String command;
+  char paddedValue[3];
+  command = START_DELIM;
+  command += VENT_MAST;
+  command += paramName;
+  sprintf(paddedValue, "%04d",
+          value);
+  command += paddedValue;
+  command += END_DELIM;
+  return command;
+}
+
+void Ctrl_Start() {
+  geCtrlState = CTRL_INIT;
+}
+
+void Ctrl_Stop() {
+  geCtrlState = CTRL_STOP;
+}
+
+bool Ctrl_StateMachine_Manager(int *sensor_data)
+{
+  bool stateChanged = false;
+  switch (geCtrlState) {
+    case CTRL_INIT:
+    {
+      Serial2.print(commands[INIT_MASTER]);
+      geCtrlState = CTRL_DO_NOTHING;
+    }
+    break;
+    case CTRL_EXPANSION_HOLD:
+    case CTRL_COMPRESSION_HOLD:
+    case CTRL_UNKNOWN_STATE:
+	case CTRL_DO_NOTHING: // Always enter this during idle scenarios.
+	break;
+    case CTRL_COMPRESSION:
+    {
+      if ((minPressureForMaskOn == false) && ((sensor_data[SENSOR_PRESSURE_A0]) > MIN_PRESSURE_FOR_MASKON )) minPressureForMaskOn = true;
+      /*When Peak Pressure Set in the UI is less than the sensor measured Peak PressureValue*/
+      if ((sensor_data[SENSOR_PRESSURE_A0] > params[PEAK_PRES].value_curr_mem) && bSendPeakHighDetected == false) {
+        bSendPeakHighDetected = true;
+        Serial2.print(commands[INH_SOLE_OFF]);
+      }
+	  geCtrlState = CTRL_DO_NOTHING;
+    }
+    break;
+    case CTRL_EXPANSION:
+    {
+      /*When Peak Pressure Set in the UI is less than the sensor measured Peak PressureValue*/
+      if ((sensor_data[SENSOR_PRESSURE_A1] < params[PEEP_PRES].value_curr_mem) && bSendPeepLowDetected == false) {
+        bSendPeepLowDetected = true;
+        Serial2.print(commands[EXH_SOLE_OFF]);
+        if ((digitalRead(AUTO_MODE))) {
+          if (check_for_dip_in_pressure(SENSOR_DP_A2)) {
+            Serial2.print(commands[INIT_BREATH_DET]);
+          }
+        }
+      } else {
+        if ((digitalRead(AUTO_MODE))) {
+          if (check_for_dip_in_pressure(SENSOR_DP_A2)) {
+		    Serial2.print(commands[INIT_BREATH_DET]);
+          }
+        }
+      }
+      geCtrlState = CTRL_DO_NOTHING;
+    }
+    break;
+    case CTRL_INHALE_DETECTION:
+    {
+      if (bBreathDetectedFlag == false) {
+        Serial.print(check_for_dip_in_pressure(SENSOR_DP_A2));
+        if (check_for_dip_in_pressure(SENSOR_DP_A2)) {
+          bBreathDetectedFlag = true;
+          Serial2.print(commands[INIT_BREATH_DET]);
+        }
+      }
+    }
+    break;
+    case CTRL_STOP:
+    {
+		Serial2.print(commands[STPR_STP]);
+		geCtrlState = CTRL_DO_NOTHING;
+    }
+    break;
+    default:
+		Serial.println("ERROR: Unknown state received in State machine");
+    break;
+  }
+  if (geCtrlPrevState != geCtrlState) {
+    geCtrlPrevState = geCtrlState;
+    bSendPeakHighDetected = false;
+    bSendPeepLowDetected = false;
+    bBreathDetectedFlag = false;
+    if (geCtrlState == CTRL_COMPRESSION ) { minPressureForMaskOn = false; }
+  }
+}
---
+++ state_control/StateControl.h
@@ -0,0 +1,162 @@
+/**************************************************************************/
+/*!
+    @file     Control_StateMachine.h
+
+    @brief     Control StateMachine Module defines the state 
+
+    @author   Tworks
+    
+  @defgroup StateControlModule  StateControlModule
+
+    Module is a stateMachine where sensors data and UI parameters data is read to take appropriate decision in the respective state. 
+  @{
+*/
+/**************************************************************************/
+
+#ifndef __STATECONTROL_H__
+#define __STATECONTROL_H__
+
+
+          
+#define STPR_STP 0                    /**< it gives the index value of the commands array  to  stop stepper motor*/
+#define STPR_ON  1                    /**< it gives the index value of the commands array  to start stepper motor*/
+#define OXY_SOLE_CYL_OFF 2            /**< it gives the index value of the commands array  to oxygen solenoid cylinder line off*/
+#define OXY_SOLE_CYL_ONN  3           /**< it gives the index value of the commands array  to oxygen solenoid cylinder line on*/
+#define OXY_SOLE_HOS_O2_OFF 4         /**< it gives the index value of the commands array  to oxygen solenoid hospital line off*/ 
+#define OXY_SOLE_HOS_O2_ONN  5        /**< it gives the index value of the commands array  to oxygen solenoid hospital line on*/
+#define INH_SOLE_OFF 6                /**< it gives the index value of the commands array  to inhalation  solenoid off*/
+#define INH_SOLE_ONN  7               /**< it gives the index value of the commands array  to inhalation  solenoid on*/
+#define EXH_SOLE_OFF 8                /**< it gives the index value of the commands array  to exhalation  solenoid off*/
+#define EXH_SOLE_ONN  9               /**< it gives the index value of the commands array  to exhalation  solenoid on*/
+#define PK_PR_REL_OFF 10              /**< it gives the index value of the commands array  to peak pressure relief off*/
+#define PK_PR_REL_ONN  11             /**< it gives the index value of the commands array  to peak pressure relief on*/
+#define SET_TID_VOL  12               /**< it gives the index value of the commands array  to set tidal volume */
+#define SET_BPM  13                   /**< it gives the index value of the commands array  to set BPM */
+#define SET_PK_PR 14                  /**< it gives the index value of the commands array  to set peak pressure*/
+#define SET_FIO2  15                   /**<it gives the index value of the commands array  to  set Fio2*/                 
+#define SET_IE_RATIO 16               /**< it gives the index value of the commands array  to set inhalation exhalation ratio */
+#define SL_EN_PARM_EDT  17            /**< it gives the index value of the commands array  to slave entering parameter edit */
+#define SL_COM_PARM_EDT 18            /**<it gives the index value of the commands array  to  slave completed parameter edit */
+#define INIT_MASTER  19               /**<it gives the index value of the commands array  to initialize master*/
+#define INIT_STPR_MOD 20              /**< it gives the index value of the commands array  to initialize stepper module*/
+#define INIT_VALV_BLK  21              /**<it gives the index value of the commands array  to  initialize stepper module*/
+#define INIT_BREATH_DET  22            /**<it gives the index value of the commands array  to  initialize breath detection*/
+
+#define SYNCH "SY"       /**< Unknown State is used when request structure is valid but makes no sense semantically */
+#define VENTSLAVE "VS"   /**< Unknown State is used when request structure is valid but makes no sense semantically */
+
+#define START_DELIM '$'  /**< Start Delimeter for the Command Structure*/
+#define END_DELIM '&'    /**< End Delimeter for the Command Structure*/
+#define VENT_MAST "VM"   /**< String to Identify Whether the request is from Ventilator Master or not*/
+#define CMD_PACKET_SIZE 10   /**< Command Packet Data Size*/
+
+#define PARAM1 "P1"       /**< String is used to Identify which parameter needs  to be Sent.In this case it is 0 param from Params Arrays */
+#define PARAM2 "P2"       /**< String is used to Identify which parameter needs  to be Sent.In this case it is 1 param from Params Arrays*/
+#define PARAM5 "P5"       /**< String is used to Identify which parameter needs  to be Sent.In this case it is 4 param from Params Arrays*/
+
+#define MIN_PRESSURE_FOR_MASKON  15
+
+/**
+ * @enum   ControlStatesDef_T
+ * @brief   Following are the Control States during which Sensor data and user inputs are compared 
+ * to generate specific commands required for that state.
+ */
+typedef enum
+{
+  CTRL_INIT=0,                /**< Initiation State*/
+  CTRL_COMPRESSION,           /**< Compression Cycle Start State */
+  CTRL_COMPRESSION_HOLD,      /**< Compression Cycle Hold State */
+  CTRL_EXPANSION,             /**< Expansion Cycle Start State */
+  CTRL_EXPANSION_HOLD,        /**< Expansion Cycle Hold State */
+  CTRL_INHALE_DETECTION,      /**< Inhale Detection State */
+  CTRL_STOP,                  /**<  Stop state  */
+  CTRL_DO_NOTHING,            /**<  Idle State */
+  CTRL_UNKNOWN_STATE,         /**< Unknown State is used when request structure is valid but makes no sense semantically */
+}ControlStatesDef_T;
+
+
+/**************************************************************************/
+/*!
+
+    @brief  Function to create control command 
+
+    @param paramName parameter is used to create command using it. 
+
+    @param value parameter defines the actual to be sent in the command
+
+    @return indicates Non-Null for SUCCESS and Null for FAILURE
+*/
+/**************************************************************************/
+String Ctrl_CreateCommand(String paramName, int value);
+/**************************************************************************/
+/*!
+
+    @brief  Function to perform specific operation based on the Current State
+
+    @return indicates true for SUCCESS and false for FAILURE
+*/
+/**************************************************************************/
+bool Ctrl_StateMachine_Manager(void);
+
+/**************************************************************************/
+/*!
+
+    @brief  Function to process the received data 
+*/
+/**************************************************************************/
+void Ctrl_ProcessRxData(void);
+/**************************************************************************/
+/*!
+
+    @brief  Function to store the bufferred data into the local variable
+
+    @param data parameter is the bufferred data 
+
+*/
+/**************************************************************************/
+void Ctrl_store_received_packet(String data);
+/**************************************************************************/
+/*!
+
+    @brief  Function to send the command from commands array based on the index
+
+    @param commandIndex parameter is used to index in the commands array
+
+
+    @return indicates 0 for SUCCESS and -1 for FAILURE
+*/
+/**************************************************************************/
+int  Ctrl_send_packet(int commandIndex);
+/**************************************************************************/
+/*!
+
+    @brief  Function to send the command based on the parameter and its value 
+
+    @param name parameter is used to send specific parameter  
+
+    @param value parameter is the actual data to be sent
+
+    @return indicates Non-Null for SUCCESS and Null for FAILURE
+*/
+/**************************************************************************/
+int  Ctrl_send_packet(String name, int value);
+/**************************************************************************/
+/*!
+
+    @brief  Function to change the state to INIT 
+
+*/
+/**************************************************************************/
+void Ctrl_Start();
+/**************************************************************************/
+/*!
+
+    @brief  Function to change the state to stop the stepper motor
+
+*/
+/**************************************************************************/
+void Ctrl_Stop();
+
+/**@}*/
+
+#endif /* __STATECONTROL_H__ */
